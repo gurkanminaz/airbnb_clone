a991bd53df76aee754bdc37723022d22c231f11a
Gürkan Minaz <gurkanminaz@gmail.com>
2024-03-03 14:35:29 -0500

    explore header

---
+++ components/Listings.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+
+interface Props {
+  listings: any[];
+  category: string;
+}
+const Listings = ({ listings, category }: Props) => {
+  useEffect(() => {
+    console.log("reload", listings.length);
+  }, [category]);
+  return (
+    <View>
+      <Text>Listings</Text>
+    </View>
+  );
+};
+
+export default Listings;
